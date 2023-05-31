c0fbd89b5f8d4ca60d6f8775d0743edc3880506a
canc90 <134351070+canc90@users.noreply.github.com>
2023-05-30 23:12:25 -0300

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: false,
+      dots: true
+    });
+  });
+  
